c851c8bb52ae97ef5740a45f5b78e587c18f5bb8
Uichan Lee <cksxkr5193@naver.com>
2024-01-25 23:31:18 +0900

    Feat: AdminBoard와 UserBoard Layout을 담당하는 BoardTemplate.tsx 구현

---
+++ Stargate-Fe/src/template/board/BoardTemplate.tsx
@@ -0,0 +1,99 @@
+import PlusButton from '@/atoms/board/PlusMinusButton';
+import BoardCardBox from '@/organisms/board/BoardCardBox';
+import BoardCardList from '@/organisms/board/BoardCardList';
+import BoardHeader from '@/organisms/board/BoardHeader';
+import { BoardData } from '@/types/board/type';
+import { Link } from 'react-router-dom';
+import { CardBoxData } from '@/types/board/type';
+
+interface BoardTemplateProps {
+  isAdmin: boolean;
+  loading: boolean;
+  data: BoardData;
+  cardBoxData: CardBoxData;
+  setExpected: boolean;
+  isExpected: boolean;
+  isFinished: boolean;
+}
+
+const BoardTemplate = ({
+  isAdmin,
+  loading,
+  data,
+  cardBoxData,
+  setExpected,
+  isExpected,
+  isFinished,
+}: BoardTemplateProps) => {
+  return (
+    <div className="w-xl min-h-screen flex flex-col justify-around">
+      <BoardHeader isAdmin={isAdmin} />
+      {loading ? (
+        <BoardCardBox
+          isAdmin={isAdmin}
+          isLoading={loading}
+          name={undefined}
+          startDate={undefined}
+        />
+      ) : (
+        cardBoxData && (
+          <BoardCardBox
+            uuid={cardBoxData.uuid}
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+            name={cardBoxData.name}
+            startDate={cardBoxData.startDate}
+            remainingTime={cardBoxData.remainingTime}
+            isAdmin={isAdmin}
+            isLoading={loading}
+          />
+        )
+      )}
+      {isAdmin && (
+        <Link to="/admin/event/create" className="fixed right-5 bottom-5 z-50">
+          <PlusButton />
+        </Link>
+      )}
+      {isExpected ? (
+        <p className="t3b text-center lg:my-14 sm:my-6 text-white">예정</p>
+      ) : (
+        <p className="t3b text-center lg:my-14 sm:my-6 text-white opacity-40">
+          예정 없음
+        </p>
+      )}
+      {loading ? (
+        <BoardCardList isLoading={loading} isAdmin={isAdmin} />
+      ) : (
+        data.expected &&
+        (setExpected ? (
+          <BoardCardList
+            meetings={data.expected.slice(1)}
+            isAdmin={isAdmin}
+            isLoading={loading}
+          />
+        ) : (
+          <BoardCardList
+            meetings={data.expected}
+            isAdmin={isAdmin}
+            isLoading={loading}
+          />
+        ))
+      )}
+      {isFinished ? (
+        <p className="t3b text-center lg:my-14 sm:my-6 text-white">리마인드</p>
+      ) : (
+        <p className="t3b text-center lg:my-14 sm:my-6 text-white opacity-40">
+          리마인드 없음
+        </p>
+      )}
+      {data.finished && (
+        <BoardCardList
+          meetings={data.finished}
+          isAdmin={isAdmin}
+          isLoading={loading}
+          isOver={true}
+        />
+      )}
+    </div>
+  );
+};
+export default BoardTemplate;
