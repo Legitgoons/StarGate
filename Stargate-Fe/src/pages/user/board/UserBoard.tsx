1802b967298c87d38f4d9f2420bc79fcd5e2cc0e
Uichan Lee <cksxkr5193@naver.com>
2024-01-25 23:37:04 +0900

    Refactor: AdminBoard와 UserBoard가 데이터를 적절히 전달하는 역할만 하도록 수정하였습니다.
    레이아웃은 BoardTemplate, 데이터를 가져오는 것은 useFetchData, 시간을 변경하는 것은 useUpdateTime이 하도록 수정했습니다.

--- Stargate-Fe/src/pages/user/board/UserBoard.tsx
+++ Stargate-Fe/src/pages/user/board/UserBoard.tsx
@@ -1,34 +1,16 @@
-import { useState, useEffect } from 'react';
-import BoardHeader from '@/organisms/board/BoardHeader';
-import BoardCardBox from '@/organisms/board/BoardCardBox';
-import BoardCardList from '@/organisms/board/BoardCardList';
-import { fetchUserBoard } from '@/services/userBoardService';
-import { BoardData } from '@/types/board/type';
+import BoardTemplate from '@/template/board/BoardTemplate';
+import { useFetchData } from '@/hooks/useFetchBoardData';
+import { useUpdateTime } from '@/hooks/useUpdateTime';
 
 const UserBoard = () => {
-  const [loading, setLoading] = useState(true);
-  const [data, setData] = useState<BoardData>({
-    ongoing: [],
-    expected: [],
-    finished: [],
-  });
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const fetchedData = await fetchUserBoard();
-      if (fetchedData) {
-        setData(fetchedData);
-      }
-      setLoading(false);
-    };
-    fetchData();
-  }, []);
+  const { loading, data, setData } = useFetchData(false);
+  useUpdateTime(data, setData);
 
   /**
-   * @param cardBoxData => CardBox에 넣을 data값 선정
-   * @param setExpected => CardBox에 Expected[0]이 들어갔는지 확인하는 변수
-   * @param isExpected => 출력할 expected값이 있는지 확인하는 변수
-   * @param isFinished => 출력할 finished값이 있는지 확인하는 변수
+   * @const {BoardData} cardBoxData => CardBox에 넣을 data값 선정
+   * @const {boolean} setExpected => CardBox에 Expected[0]이 들어갔는지 확인하는 변수
+   * @const {boolean} isExpected => 출력할 expected값이 있는지 확인하는 변수
+   * @const {boolean} isFinished => 출력할 finished값이 있는지 확인하는 변수
    */
   const cardBoxData = data.ongoing[0] || data.expected[0];
   const setExpected = cardBoxData === data.expected[0];
@@ -37,98 +19,16 @@ const UserBoard = () => {
     (setExpected === false && data.expected[0] !== undefined);
   const isFinished = data.finished[0] !== undefined;
 
-  /**
-   * @todo => 추후에 useInterval로 수정
-   */
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setData((prevData) => ({
-        ...prevData,
-        ongoing: prevData.ongoing.map((meeting) => {
-          if (meeting.remainingTime > 0) {
-            return {
-              ...meeting,
-              remainingTime: meeting.remainingTime - 1,
-            };
-          }
-          return meeting;
-        }),
-        expected: prevData.expected.map((meeting) => {
-          if (meeting.remainingTime > 0) {
-            return {
-              ...meeting,
-              remainingTime: meeting.remainingTime - 1,
-            };
-          }
-          return meeting;
-        }),
-      }));
-    }, 1000); // 1초에 1씩 줄어듬
-
-    return () => clearInterval(interval); // 메모리 누수 방지를 위해 clearInterval
-  }, [data]);
-
   return (
-    <div className="w-xl min-h-screen flex flex-col justify-around">
-      <BoardHeader isAdmin={false} />
-      {loading ? (
-        <BoardCardBox isAdmin={false} isLoading={loading} />
-      ) : (
-        cardBoxData && (
-          <BoardCardBox
-            uuid={cardBoxData.uuid}
-            imageSrc={cardBoxData.imageFileInfo?.fileUrl}
-            title={cardBoxData.name}
-            date={cardBoxData.startDate}
-            remainingTime={cardBoxData.remainingTime}
-            isAdmin={false}
-            isLoading={loading}
-          />
-        )
-      )}
-      {isExpected ? (
-        <p className="t3b text-center lg:my-14 sm:my-6 text-white">예정</p>
-      ) : (
-        <p className="t3b text-center lg:my-14 sm:my-6 text-white opacity-40">
-          예정 없음
-        </p>
-      )}
-      {loading ? (
-        <BoardCardList isLoading={loading} isAdmin={false} />
-      ) : (
-        data.expected &&
-        (setExpected ? (
-          <BoardCardList
-            meetings={data.expected.slice(1)}
-            isAdmin={false}
-            isLoading={loading}
-          />
-        ) : (
-          <BoardCardList
-            meetings={data.expected}
-            isAdmin={false}
-            isLoading={loading}
-          />
-        ))
-      )}
-      {isFinished ? (
-        <p className="t3b text-center lg:my-14 sm:my-6 text-white">리마인드</p>
-      ) : (
-        <p className="t3b text-center lg:my-14 sm:my-6 text-white opacity-40">
-          리마인드 없음
-        </p>
-      )}
-
-      {data.finished && (
-        <BoardCardList
-          meetings={data.finished}
-          isAdmin={false}
-          isLoading={loading}
-          isOver={true}
-        />
-      )}
-    </div>
+    <BoardTemplate
+      isAdmin={false}
+      loading={loading}
+      data={data}
+      cardBoxData={cardBoxData}
+      setExpected={setExpected}
+      isExpected={isExpected}
+      isFinished={isFinished}
+    />
   );
 };
 
