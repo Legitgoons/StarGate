37dbaa3053a928505d0ea73bad4be90d5fbde81e
Uichan Lee <cksxkr5193@naver.com>
2024-03-18 20:43:50 +0900

    Feat: browserState값에 따라 useFetchBoardData 분기처리
    browserState 값이 false일 때만 Page Visibility API로 데이터를 다시 불러오도록 했습니다.
    또한 함수명을 useFetchData에서 useFetchBoardData로 수정했습니다.

--- Stargate-Fe/src/hooks/useFetchBoardData.ts
+++ Stargate-Fe/src/hooks/useFetchBoardData.ts
@@ -1,15 +1,18 @@
 import { useState, useEffect } from 'react';
+import { useRecoilValue } from 'recoil';
+import { browserState } from '@/recoil/browserState';
 import { BoardData } from '@/types/board/type';
 import { fetchUserBoard } from '@/services/userBoardService';
 import { fetchAdminBoard } from '@/services/adminBoardService';
 
-export const useFetchData = (isAdmin: boolean) => {
+export const useFetchBoardData = (isAdmin: boolean) => {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState<BoardData>({
     ongoing: [],
     expected: [],
     finished: [],
   });
+  const isV8 = useRecoilValue(browserState);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,18 +25,23 @@ export const useFetchData = (isAdmin: boolean) => {
       setLoading(false);
     };
 
-    const handleVisibilityChange = () => {
-      if (document.visibilityState === 'visible') {
-        fetchData();
-      }
-    };
-    fetchData();
+    if (!isV8) {
+      const handleVisibilityChange = () => {
+        if (document.visibilityState === 'visible') {
+          fetchData();
+        }
+      };
+      fetchData();
 
-    document.addEventListener('visibilitychange', handleVisibilityChange);
-    return () => {
-      document.removeEventListener('visibilitychange', handleVisibilityChange);
-    };
-  }, []);
+      document.addEventListener('visibilitychange', handleVisibilityChange);
+      return () => {
+        document.removeEventListener(
+          'visibilitychange',
+          handleVisibilityChange
+        );
+      };
+    }
+  }, [isV8]);
 
   return { loading, data, setData };
 };
