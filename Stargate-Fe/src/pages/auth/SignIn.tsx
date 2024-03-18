aa54d5177be53e936090f9776e5cefd71d3487ad
Uichan Lee <cksxkr5193@naver.com>
2024-03-18 20:32:06 +0900

    Feat: signIn 페이지와 Board 템플릿에서 os/브라우저 판별 구현

--- Stargate-Fe/src/pages/auth/SignIn.tsx
+++ Stargate-Fe/src/pages/auth/SignIn.tsx
@@ -1,23 +1,19 @@
 import React, { useEffect } from 'react';
+import { useSetRecoilState } from 'recoil';
+import { browserState } from '@/recoil/browserState';
 import SignInComponent from '@/organisms/auth/SignInComponent';
 import ToolTipComponent from '@/atoms/auth/ToolTipComponent';
-import Swal from 'sweetalert2';
+import { browserAlert } from '@/utils/browserAlert';
 
 const SignIn = () => {
+  const setBrowser = useSetRecoilState(browserState);
+  const userAgent = window.navigator.userAgent;
+
   useEffect(() => {
-    const userAgent = window.navigator.userAgent;
-    if (userAgent.includes('Mac')) {
-      Swal.fire({
-        icon: 'warning',
-        title: '운영체제 권장 안내',
-        text: '최적의 실행환경을 위해 Windows 운영체제를 권장합니다.',
-      });
-    } else if (userAgent.includes('Firefox')) {
-      Swal.fire({
-        icon: 'warning',
-        title: '브라우저 권장 안내',
-        text: '최적의 사용을 위해 Chrome 혹은 Edge 브라우저를 권장합니다.',
-      });
+    console.log(userAgent);
+    if (userAgent.includes('Mac') || userAgent.includes('Firefox')) {
+      setBrowser(false);
+      browserAlert(userAgent);
     }
   }, []);
 
